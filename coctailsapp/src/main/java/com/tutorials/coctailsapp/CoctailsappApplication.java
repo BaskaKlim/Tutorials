a428d61b6938bbfbbb8182a9612c1d9b53ffcf3e
BaskaKlim <baska.klimekova@gmail.com>
2020-04-19 22:11:14 +0200

    Spring Initializr

---
+++ coctailsapp/src/main/java/com/tutorials/coctailsapp/CoctailsappApplication.java
@@ -0,0 +1,13 @@
+package com.tutorials.coctailsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoctailsappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoctailsappApplication.class, args);
+	}
+
+}
