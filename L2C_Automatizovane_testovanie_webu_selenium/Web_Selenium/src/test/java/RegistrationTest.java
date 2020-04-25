570e387f1ed2404e2faae44365dd87457d9b8175
BaskaKlim <baska.klimekova@gmail.com>
2020-04-25 19:57:33 +0200

    using findElement by.CssSelector

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/RegistrationTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/RegistrationTest.java
@@ -27,16 +27,14 @@ public class RegistrationTest {
         driver.findElement(By.name("surname")).sendKeys("klimek");
         driver.findElement(By.name("password")).sendKeys("1234");
         driver.findElement(By.name("password-repeat")).sendKeys("1234");
-
-
-    
+        driver.findElement(By.cssSelector("input#checkbox")).click();
     }
 
 
 
     @After
     public void tearDown() throws Exception {
-        driver.quit();
+       // driver.quit();
         String verificationErrorString = verificationErrors.toString();
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
