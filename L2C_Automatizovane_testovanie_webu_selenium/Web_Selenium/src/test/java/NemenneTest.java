708ca31c794993cc68577d7126cecba1f4f2d4de
BaskaKlim <baska.klimekova@gmail.com>
2020-05-14 23:25:09 +0200

    prepare template - webdriver, URL, before and after test

---
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/NemenneTest.java
@@ -0,0 +1,35 @@
+import java.util.concurrent.*;
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+import static org.junit.Assert.*;
+
+public class NemenneTest {
+
+    private WebDriver driver;
+    final private String BASE_URL = "http://localhost";
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        //relative path cize od tohto projektu
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.get(BASE_URL + ":8888/nemenne.php");
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+
+    }
+
+
+}
