d433b33b189125f837c1034ba79edb987073fa4a
BaskaKlim <baska.klimekova@gmail.com>
2020-05-14 22:58:41 +0200

    using alert.sendKeys to write values to alert window form

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/AlertTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/AlertTest.java
@@ -51,11 +51,26 @@ public class AlertTest {
         driver.findElement(By.id("alert2")).click();
         // switch to alert window
         Alert alert = driver.switchTo().alert();
+        //print text on alert window
+        System.out.println(alert.getText());
         //dismiss alert
         alert.dismiss();
         //assert text message is "Negative"
         String text = driver.findElement(By.xpath("//div[@class='result']/h1")).getText();
         Assert.assertEquals(text, "Negative");
-        
+
     }
+
+    @Test
+    // TODO: overit, ci sa zobrazi hlaska po kliknuti na button 3 a vpisani vstupnych parametrov do alert okna
+    public void itShouldRunAlert3() {
+        driver.findElement(By.id("alert3")).click();
+        Alert alert = driver.switchTo().alert();
+        // write key value that the alert ask for
+        alert.sendKeys("Yuri");
+        alert.accept();
+        String text = driver.findElement(By.xpath("//div[@class='result']/h1")).getText();
+        Assert.assertTrue(text.contains("Yuri"));
+    }
+
 }
