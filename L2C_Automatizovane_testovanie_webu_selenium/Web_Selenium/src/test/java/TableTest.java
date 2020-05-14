3454f281feef11faca6a758cc80477a68b70fc7c
BaskaKlim <baska.klimekova@gmail.com>
2020-05-14 23:57:47 +0200

    difference between assert and verification, try-cacth blok

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/TableTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/TableTest.java
@@ -53,16 +53,28 @@ public class TableTest {
 
     @Test
 
-    public void itShouldPrintAllRows2(){
-        int rows = Integer.parseInt(driver.findElement(By.xpath("//table/tbody/tr[(last())]/td[1]")).getText()) ;
-      // od 1. riadku po pocet riadkov rows
-    for (int i=1; i<rows; i++){
-        System.out.println(driver.findElement(By.xpath("//table/tbody/tr["+i+"]")).getText());
+    public void itShouldPrintAllRows2() {
+        int rows = Integer.parseInt(driver.findElement(By.xpath("//table/tbody/tr[(last())]/td[1]")).getText());
+        // od 1. riadku po pocet riadkov rows
+        for (int i = 1; i < rows; i++) {
+            System.out.println(driver.findElement(By.xpath("//table/tbody/tr[" + i + "]")).getText());
         }
 
-    Assert.assertFalse(driver.findElement(By.xpath("//table/tbody/tr[3]")).getText().isEmpty());
+        Assert.assertFalse(driver.findElement(By.xpath("//table/tbody/tr[3]")).getText().isEmpty());
     }
-    
+
+    @Test
+    public void testVerify() {
+        //assert
+        Assert.assertEquals("1", driver.findElement(By.cssSelector("td")).getText());
+
+        //verify, zachyti chybu do catch bloku a vyhodi ju az na konci
+        try {
+            Assert.assertEquals("2", driver.findElement(By.xpath("//tr[2]/td")).getText());
+        } catch (Error e) {
+            verificationErrors.append(e.toString());
+        }
+    }
 
     @After
     public void tearDown() throws Exception {
