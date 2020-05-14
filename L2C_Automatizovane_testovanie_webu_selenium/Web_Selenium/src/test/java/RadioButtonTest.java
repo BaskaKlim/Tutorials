2672cd2aeee8e8cb40a7f2325db74b204067980d
BaskaKlim <baska.klimekova@gmail.com>
2020-05-14 23:20:58 +0200

    validation of correct radioButton selection, via assertTrue and assertFalse, using click(),isSelected() functions

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/RadioButtonTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/RadioButtonTest.java
@@ -1,4 +1,5 @@
 import java.util.concurrent.*;
+import javax.xml.soap.*;
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
@@ -30,4 +31,26 @@ public class RadioButtonTest {
         }
 
     }
+
+    @Test
+    //TODO: overit moznost MUZ
+    public void itShouldChoseOptionMuz() {
+
+        //1. find input a pristupit jeho value pre moznos muz //2. zakliknut tento radiobutton
+        driver.findElement(By.xpath("//input[@value='wurst']")).click();
+        //2. vytiahnut si text po zobrazeni
+        String message = driver.findElement(By.xpath("//h1[@class='description text-center']")).getText();
+        //3. overit ocakavanu hodnotu
+        Assert.assertEquals(message, "It's wurst");
+    }
+
+    @Test
+    //TODO: overit spravnost zaclicknutia radiobuttonov, moze byt vzdy len jeden z nich vybrany
+    public void itShouldValidateCorrectChoosingOfRadioButtons() {
+
+        driver.findElement(By.xpath("//input[@value='wurst']")).click();
+        Assert.assertTrue(driver.findElement(By.xpath("//input[@value='wurst']")).isSelected());
+        Assert.assertFalse(driver.findElement(By.xpath("//input[@value='conchita']")).isSelected());
+    }
+
 }
