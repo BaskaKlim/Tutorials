829503049430d2cf033419af895939288041a7b6
BaskaKlim <baska.klimekova@gmail.com>
2020-05-14 23:36:17 +0200

    validation if elements are enabled or not, form input fields and dropdown itself, and dropdown options

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/NemenneTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/NemenneTest.java
@@ -28,8 +28,23 @@ public class NemenneTest {
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
         }
-
     }
 
+    @Test
+
+    public void itShouldValidateThatElemensOfFormAreOrAreNotEnabled() {
+        //first input field
+        Assert.assertFalse(driver.findElement(By.xpath("//input[@value='Eva Mazikova']")).isEnabled());
+        //second input field
+        Assert.assertFalse(driver.findElement(By.id("sel1")).isEnabled());
+        //third input field   - dropdown, so at first chect whole element, that the options
+        Assert.assertTrue(driver.findElement(By.id("sel2")).isEnabled());
+        //first 2 should be not enabled and 3. shoudl be enabled
+        Assert.assertFalse(driver.findElement(By.id("sel2")).findElement(By.xpath("./option[1]")).isEnabled());
+        Assert.assertFalse(driver.findElement(By.id("sel2")).findElement(By.xpath("./option[2]")).isEnabled());
+        Assert.assertTrue(driver.findElement(By.id("sel2")).findElement(By.xpath("./option[3]")).isEnabled());
+        
+
+    }
 
 }
