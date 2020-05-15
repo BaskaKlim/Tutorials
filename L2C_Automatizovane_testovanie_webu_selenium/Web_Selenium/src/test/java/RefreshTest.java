4c21a90e8a0cb79140960176b657c898b052744b
BaskaKlim <baska.klimekova@gmail.com>
2020-05-15 20:30:58 +0200

    refreshTest

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/RefreshTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/RefreshTest.java
@@ -1,10 +1,12 @@
 import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
+import org.openqa.selenium.support.ui.*;
 
 import static org.junit.Assert.*;
 
 public class RefreshTest {
+
     private WebDriver driver;
     final private String BASE_URL = "http://localhost:8888/";
     private StringBuffer verificationErrors = new StringBuffer();
@@ -23,6 +25,24 @@ public class RefreshTest {
         if (!"".equals(verificationErrorString)) {
             fail(verificationErrorString);
         }
+    }
+
+    @Test
+    public void itShouldRefreshPage() {
+        driver.get(BASE_URL + "clickmebaby.php");
+
+        //click 5 time on the button and validate that there is 5 clicks made
+        for (int i = 0; i < 5; i++) {
+            driver.findElement(By.id("clickMe")).click();
+        }
+        Assert.assertEquals("5", driver.findElement(By.id("clicks")).getText());
+
+        // refresh    and wait for refreshed page
+        driver.navigate().refresh();
+        new WebDriverWait(driver, 10).until(ExpectedConditions.presenceOfElementLocated(By.id("clicks")));
+
+        //validate that there is 0 clicks made
+        Assert.assertEquals("0", driver.findElement(By.id("clicks")).getText());
     }
 
 }
