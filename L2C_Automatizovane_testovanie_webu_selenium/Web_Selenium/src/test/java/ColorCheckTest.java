80d1617d5e8bce1dbaeaa80883537e06c911c9db
BaskaKlim <baska.klimekova@gmail.com>
2020-05-15 16:10:25 +0200

    test template for colorcheck test

---
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/ColorCheckTest.java
@@ -0,0 +1,29 @@
+import org.junit.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.*;
+
+import static org.junit.Assert.*;
+
+public class ColorCheckTest {
+    private WebDriver driver;
+    final private String BASE_URL = "http://localhost:8888/";
+    private StringBuffer verificationErrors = new StringBuffer();
+
+    @Before
+    public void setUp() throws Exception {
+        //relative path cize od tohto projektu
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
+        driver = new ChromeDriver();
+        driver.get(BASE_URL + "stroopeffect.php");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+}
