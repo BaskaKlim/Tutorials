59a88d3b16e57d375eb9cf03dba3bc60ee2f1001
BaskaKlim <baska.klimekova@gmail.com>
2020-05-15 15:03:37 +0200

    testing hover state of current webpage in navigation via checking class atribute is active

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/NavBarTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/NavBarTest.java
@@ -2,6 +2,8 @@ import org.junit.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 
+import static org.junit.Assert.*;
+
 public class NavBarTest {
 
     private WebDriver driver;
@@ -13,6 +15,24 @@ public class NavBarTest {
         //relative path cize od tohto projektu
         System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver");
         driver = new ChromeDriver();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        driver.quit();
+        String verificationErrorString = verificationErrors.toString();
+        if (!"".equals(verificationErrorString)) {
+            fail(verificationErrorString);
+        }
+    }
+
+    @Test
+    //TODO: over ci je vyznacena podstranka na navigacii ta, na ktorej sa prave nachadzas
+
+    public void itShouldMarkTheCurrecntPageInNavigation(){
+        driver.get(BASE_URL + ":8888/zjavenie.php");
+        Assert.assertTrue(driver.findElement(By.xpath("//li[a/@href='zjavenie.php']")).getAttribute("class").contains("active"));
+
     }
 
 }
