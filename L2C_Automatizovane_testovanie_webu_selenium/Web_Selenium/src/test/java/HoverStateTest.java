90e43ed42f71ba5aedf7a23c6be762eda8099777
BaskaKlim <baska.klimekova@gmail.com>
2020-05-17 21:35:09 +0200

    using selenium class actions and method clickAndHold(), build and perform()

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/HoverStateTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/HoverStateTest.java
@@ -28,19 +28,25 @@ public class HoverStateTest {
         //0.expected values - these we are testing
         String expectedGreen = "rgba(10, 129, 0, 1)";
         String expectedRed = "rgba(205, 58, 63, 1)";
+        String expectedOrange = "rgba(191, 111, 7, 1)";
 
         //1.vytiahnem si element
         WebElement trafficLights = driver.findElement(By.className("light"));
         //2. vychodzia farba  a overim ocakavanu farbu s realnou
         String actualRedColor = (trafficLights.getCssValue("background-color"));
         Assert.assertEquals(expectedRed,actualRedColor);
+
         //3. akcia - prechod mysov, hover stav
         Actions actions = new Actions(driver);
         actions.moveToElement(trafficLights).build().perform();
-        //4. vytiahnem si aktualnu farbu
+        //4. vytiahnem si aktualnu farbu a porovnam ocakavnu a aktualnu farbu
         String actualGreenColor = (trafficLights.getCssValue("background-color"));
-        //5. porovnam ocakavnu a aktualnu farbu
         Assert.assertEquals(expectedGreen,actualGreenColor);
+
+        //5. akcia - podrzanie mysi,  pri action musi byt vzdy build a perform
+        actions.clickAndHold(trafficLights).build().perform();
+        String actualOrangeColor = (trafficLights.getCssValue("background-color"));
+        Assert.assertEquals(expectedOrange,actualOrangeColor);
     }
    
 }
