0506f38ebdec05ec850e5d4c40b135a5ef463d9a
BaskaKlim <baska.klimekova@gmail.com>
2020-05-17 21:55:30 +0200

    DragAndDrop test

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/DragAndDropElementTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/DragAndDropElementTest.java
@@ -26,15 +26,18 @@ public class DragAndDropElementTest {
 
     @Test
     public void test() {
+        //vybranie elementov co chceme presunut a kam chceme presunut
         WebElement donald = driver.findElement(By.id("donald"));
         WebElement tree = driver.findElement(By.id("tree"));
-
+        //actions class zo selenia a parametere pre dragAndDrop
         Actions actions = new Actions(driver);
         actions.dragAndDrop(donald, tree).build().perform();
 
+        //zadefinovanie si hladaneho a aktualneho elementu
         String expectedMessage = "HOOO HOOOOO !!!!";
         WebElement message = driver.findElement(By.xpath("//div[contains(@class,'success')]/h2"));
 
+        //konkretne veci co overujem
         Assert.assertTrue(message.isDisplayed());
         Assert.assertEquals(expectedMessage, message.getText());
     }
