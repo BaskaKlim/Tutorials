7f4a8a71b2814be5787109baed555774442c5c24
BaskaKlim <baska.klimekova@gmail.com>
2020-05-18 12:00:18 +0200

    simple calculation tests

--- L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/CalculatorTest.java
+++ L2C_Automatizovane_testovanie_webu_selenium/Web_Selenium/src/test/java/CalculatorTest.java
@@ -3,48 +3,81 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.*;
 
 public class CalculatorTest {
+
     private WebDriver driver;
     private final String BASE_URL = "http://localhost:8888/kalkulacka.php";
 
     @Before
 
-    public void setUp(){
+    public void setUp() {
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get(BASE_URL);
     }
 
-
     @After
 
-    public void tearDown(){
+    public void tearDown() {
         driver.close();
         driver.quit();
     }
 
     @Test
-    public void testSum(){
+    public void testSum() {
+        //actions
+        driver.findElement(By.id("firstInput")).sendKeys("6");
+        driver.findElement(By.id("secondInput")).sendKeys("2");
+        driver.findElement(By.id("count")).click();
+        // validation
+        Assert.assertTrue(driver.findElement(By.id("result")).isDisplayed());
+        Assert.assertEquals("8", driver.findElement(By.id("result")).getText());
+
 
     }
 
     @Test
-    public void testSub(){
-
-    }
-    @Test
-    public void testMultiply(){
-
-    }
-
-    @Test
-    public void testDivide(){
+    public void testSub() {
+        //actions
+        driver.findElement(By.id("firstInput")).sendKeys("6");
+        driver.findElement(By.id("secondInput")).sendKeys("2");
+        driver.findElement(By.id("count")).click();
+        // validation
+        Assert.assertTrue(driver.findElement(By.id("result")).isDisplayed());
+        Assert.assertEquals("4", driver.findElement(By.id("result")).getText());
 
     }
 
     @Test
-    public void testInvalidInputs(){
+    public void testMultiply() {
+        //actions
+        driver.findElement(By.id("firstInput")).sendKeys("6");
+        driver.findElement(By.id("secondInput")).sendKeys("2");
+        driver.findElement(By.id("count")).click();
+        // validation
+        Assert.assertTrue(driver.findElement(By.id("result")).isDisplayed());
+        Assert.assertEquals("12", driver.findElement(By.id("result")).getText());
+
 
     }
-    
+
+    @Test
+    public void testDivide() {
+        //actions
+        driver.findElement(By.id("firstInput")).sendKeys("6");
+        driver.findElement(By.id("secondInput")).sendKeys("2");
+        driver.findElement(By.id("count")).click();
+        // validation
+        Assert.assertTrue(driver.findElement(By.id("result")).isDisplayed());
+        Assert.assertEquals("3", driver.findElement(By.id("result")).getText());
+
+
+    }
+
+    @Test
+    public void testInvalidInputs() {
+        driver.findElement(By.id("firstInput"));
+        driver.findElement(By.id("secondInput"));
+
+    }
 
 }
